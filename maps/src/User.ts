d00db501d5700c209fd8ef6e2c356c04fcc46895
dineshkashera <dineshk@one.com>
2024-11-30 17:42:42 +0530

    resolved faker types error

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,3 +1,4 @@
+import {  fakerEN } from '@faker-js/faker';
 export class User{
   name:string;
   location:{
@@ -6,10 +7,11 @@ export class User{
   };
 
   constructor(){
-      this.name = 'Hello';
+    
+      this.name = fakerEN.person.fullName();
       this.location = {
-        lat: parseFloat('23.44'),
-        lng: parseFloat('24.55')
+        lat: fakerEN.location.latitude(),
+        lng: fakerEN.location.longitude()
       }
 
       console.log(`${this.name} - ${JSON.stringify(this.location)}`);
